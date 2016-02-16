caf037e4a0892451f64e4847b7e82f698baab19f
Lucas LeVieux <contact@lucaslevieux.com>
2016-02-15 22:13:42 -0800

    Rewrote to use the Servo class.  Now it actually works.
    See http://www.chiefdelphi.com/forums/showthread.php?t=137455

--- PWMTester.ino
+++ PWMTester.ino
@@ -1,9 +1,13 @@
-const int pins[] = {5, 6}; //must be 3, 5, 6, 9, 10, or 11
+#include <Servo.h>
+
 int value = 0;
+Servo controller1, controller2;
 
 void setup() {
   Serial.begin(9600);
   Serial.println("you called, master?\n");
+  controller1.attach(5); //associate object with pin
+  controller2.attach(6);
   delay(1000);
   Serial.println("type in a percent, and I will output your PWM.\n");
   
@@ -11,21 +15,22 @@ void setup() {
 
 
 void loop() {
-  int PWMvalue = value * (double)255/(double)100; //scale up to 255
-  
-  for (int i=0; i<sizeof(pins); i++) {
-    analogWrite(pins[i], value);
-  }
+  int PWMvalue = value * 5 + 1500; //scale up to 1000-2000
+
+  controller1.writeMicroseconds(PWMvalue);
+  controller2.writeMicroseconds(PWMvalue);
 
   if (Serial.available() > 1) {
     long proposedValue = Serial.parseInt();
-    if (proposedValue >= -100 && proposedValue <= 100) {
+    if (proposedValue == 0) {
+      Serial.println("ah... stop!  yes, right on it.");
+      value = 0;
+    } else if (proposedValue >= -100 && proposedValue <= 100) {
       value = proposedValue;
       Serial.print("of course. value set to ");
       Serial.print(value);
       Serial.println("%");
-      }
-    else {
+    } else {
       Serial.println("oh dear. that won't do.");
     }
   }
