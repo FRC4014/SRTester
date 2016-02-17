f182c09f43b211a4eb20ff04fb9cddb85d5f0a99
Lucas LeVieux <contact@lucaslevieux.com>
2016-02-16 16:26:08 -0800

    add support for more pins + GPL liscence

--- PWMTester.ino
+++ PWMTester.ino
@@ -1,13 +1,29 @@
+/* This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <Servo.h>
 
-int value = 0;
-Servo controller1, controller2;
+int percent = 0;  //between -100 and 100, indicates how fast the motor will be moving
+int pins[] = {5, 6}; //the signal output pins (as many as you'd like)
+
+
+Servo controllers[sizeof(pins)];
 
 void setup() {
   Serial.begin(9600);
   Serial.println("you called, master?\n");
-  controller1.attach(5); //associate object with pin
-  controller2.attach(6);
+  for (int i=0; i<sizeof(controllers); i++)
+    controllers[i].attach(pins[i]); //associate the object to a pin
   delay(1000);
   Serial.println("type in a percent, and I will output your PWM.\n");
   
@@ -15,20 +31,20 @@ void setup() {
 
 
 void loop() {
-  int PWMvalue = value * 5 + 1500; //scale up to 1000-2000
+  int PWMvalue = percent * 5 + 1500; //scale up to 1000-2000
 
-  controller1.writeMicroseconds(PWMvalue);
-  controller2.writeMicroseconds(PWMvalue);
+  for (int i=0; i<sizeof(controllers); i++)
+    controllers[i].writeMicroseconds(PWMvalue);
 
   if (Serial.available() > 1) {
     long proposedValue = Serial.parseInt();
     if (proposedValue == 0) {
       Serial.println("ah... stop!  yes, right on it.");
-      value = 0;
+      percent = 0;
     } else if (proposedValue >= -100 && proposedValue <= 100) {
-      value = proposedValue;
+      percent = proposedValue;
       Serial.print("of course. value set to ");
-      Serial.print(value);
+      Serial.print(percent);
       Serial.println("%");
     } else {
       Serial.println("oh dear. that won't do.");
